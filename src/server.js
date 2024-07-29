902c427d8879d77d96ed091d827a55a1a89df7b0
alisaid-sm <alisaid7170@gmail.com>
2024-07-29 10:08:59 +0700

    done album api

---
+++ src/server.js
@@ -0,0 +1,52 @@
+require("dotenv").config();
+
+const Hapi = require("@hapi/hapi");
+const ClientError = require("./exceptions/ClientError");
+const AlbumsService = require("./services/AlbumsServices");
+const albums = require("./api/albums");
+const AlbumsValidator = require("./validator/albums");
+
+const init = async () => {
+  const albumsService = new AlbumsService();
+
+  const server = Hapi.server({
+    port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ["*"],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: albums,
+    options: {
+      service: albumsService,
+      validator: AlbumsValidator,
+    },
+  });
+
+  await server.start();
+
+  server.ext("onPreResponse", (request, h) => {
+    // mendapatkan konteks response dari request
+    const { response } = request;
+
+    // penanganan client error secara internal.
+    if (response instanceof ClientError) {
+      const newResponse = h.response({
+        status: "fail",
+        message: response.message,
+      });
+      newResponse.code(response.statusCode);
+      return newResponse;
+    }
+
+    return h.continue;
+  });
+
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
